11fabd960fa6db0cb1ed08ce861c9506c382c365
Jenkins Chinwor <90245242+jchinwor@users.noreply.github.com>
2024-03-31 21:16:45 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template> 
 <Header />
-<router-view />
+<RouterView v-slot="{ Component }" class="content" :key="route.fullPath">
+  <Transition :name="(route?.meta?.transition as string) || 'fade'" mode="out-in" appear>
+    <component :is="Component" />
+  </Transition>
+</RouterView>
 <Footer />
 </template>
 
 <script setup>
+import { RouterView, useRoute } from 'vue-router'
+const route = useRoute()
 import NavBar from './components/NavBar.vue'
 import Header from './components/Header.vue'
 import Navigation from './components/Navigation.vue'
