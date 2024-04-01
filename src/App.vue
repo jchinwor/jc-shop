5ab105589a58b656eea6725ab1e68cf7c39970c9
Jenkins Chinwor <90245242+jchinwor@users.noreply.github.com>
2024-04-01 10:43:32 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template> 
 <Header />
-<router-view />
+<router-view v-slot="{ Component }">
+  <transition name="slide" >
+    <component :is="Component" />
+  </transition>
+</router-view>
 <Footer />
 </template>
 
