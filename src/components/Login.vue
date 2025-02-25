6605ff2f46009ead4030f425b7f395559d479ddc
Jenkins Chinwor <90245242+jchinwor@users.noreply.github.com>
2025-02-25 17:21:42 +0000

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card mt-5">
-     <Toast position="top-center" />
+     <Toast position="top-center" group="tc" />
         <form @submit.prevent="onSubmit">
             
             <Fieldset legend="Login">
@@ -96,13 +96,13 @@ export default {
 
         if(email.value === "" ){
         
-         toast.add({ severity: 'error', summary: 'Email is required', detail: '', life: 6000 });
+         toast.add({ severity: 'error', summary: 'Email is required', detail: '',group: 'tc', life: 6000 });
         
                 // resetForm();
             }
             else if(password.value === "" ) {
                 
-                toast.add({ severity: 'error', summary: 'Password is required',detail: '', life: 5000 });
+                toast.add({ severity: 'error', summary: 'Password is required',detail: '',group: 'tc', life: 5000 });
                 // resetForm();
             }else{
 
