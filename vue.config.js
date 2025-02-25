0abfc1aabbbaa48a8c6200dd91eb73c8f88b5493
Jenkins Chinwor <90245242+jchinwor@users.noreply.github.com>
2025-02-25 18:57:05 +0000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/',
+  outputDir: 'dist',
   devServer:{
     // proxy: 'http://localhost:8000',
     proxy: {
